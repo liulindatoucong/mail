f7cb3a0a5a45734fe959d1d3a5e360bad2461450
liulin <liulindatoucong@163.com>
2023-02-20 16:44:07 +0800

    发送邮件的一个初步图像

---
+++ src/com/liulin/MainFrame.java
@@ -0,0 +1,25 @@
+package com.liulin;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class MainFrame extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("weimail.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
